5df63573308c2bd7b9798f2cfb4a44a8f86b5742
Abraam Ibrahim <87959474+EvoL1e@users.noreply.github.com>
2021-10-17 15:42:02 -0700

    Added the licenses, license badges, and links to the licenses in the generateMarkdown.js and added some questions to be asked to the use.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project's name?", 
+"What would you like to add to the description of your project?",
+"Would you like a table of contents?",
+"What are the steps required for someone to install your project?",
+"What are some use cases and examples of your project?",
+"If there are any, who helped you in the creation of your project, who were they?",
+"Which License would you like to use for your project?",
+"How can someone contribute to your project?",
+"How can someone else run a test of your project?",
+"What is your GitHub link?",
+"What is your email?",
+"What is your first and last name?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
