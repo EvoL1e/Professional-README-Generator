2375329a1566f4bff8c5cbcbb904427fadefeca3
Abraam Ibrahim <87959474+EvoL1e@users.noreply.github.com>
2021-10-17 19:04:39 -0700

    Created the user prompter

--- Develop/index.js
+++ Develop/index.js
@@ -20,7 +20,76 @@ const questions = ["What is your project's name?", // Project Title
 
 // TODO: Make a function that will prompt the user
 function promptUser() {
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your projects name?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What would you like to add to the description of your project?',
+            name: 'description'
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like a table of contents?',
+            name: 'tableOfContents'
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required for someone to install your project?',
+            name: 'install'
+        },
+        {
+            type: 'input',
+            message: 'What are some use cases and examples of your project?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'If there are any, who helped you in the creation of your project, who were they?',
+            name: 'credits'
+        },
+        {
+            type: 'list',
+            message: 'Which License would you like to use for your project?',
+            name: 'usage',
+            choices: 'Apache License 2.0, GNU GPLv2, GNU GPLv3, MIT, ISC'
+        },
+        {
+            type: 'input',
+            message: 'How can someone contribute to your project and what are the guidelines?',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'How can someone else run a test of your project?',
+            name: 'test'
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub link?',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your first and last name?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is the current year?',
+            name: 'year'
+        },
+        
+    ])
 }
 
 // TODO: Create a function to write README file
